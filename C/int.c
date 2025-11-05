fcdd31f064132091f50619246832ffb1f15898ba
unknown User 404 <Bhavneet11d@gmail.com>
2025-11-04 23:35:57 -0500

    Create int.c

---
+++ C/int.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a =4+9/10;
+    printf("Value of a is ");
+    printf(" %d\n " ,a);
+    printf("Try programiz.pro");
+
+    return 0;
+}
