7f0b3c8b830b129998a8c0847e1244046faa4fec
unknown User 404 <Bhavneet11d@gmail.com>
2025-11-04 23:35:30 -0500

    Create float.c

---
+++ C/float.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    float a =4+9/10;
+    printf("Value of a is ");
+    printf(" %f\n " ,a);
+    printf("Try programiz.pro");
+
+    return 0;
+}
