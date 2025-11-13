b28ec1408bc255de070f5d4723008f229bed88bc
unknown User 404 <Bhavneet11d@gmail.com>
2025-11-12 23:36:33 -0500

    Add pi_value.c to print the value of pi

---
+++ C/Variable_and_Identifier/Double/pi_value.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    // Initialize a double variable
+    double PI = 3.1415;
+
+   // Use %lf to print a double
+   printf("The value of pi is %lf", PI); //use %lf (stands for long float)
+
+   return 0;
+}
