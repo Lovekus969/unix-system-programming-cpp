f648cbeef786ddc46bd78d3e5f78cc8a481c8281
unknown User 404 <Bhavneet11d@gmail.com>
2025-11-12 23:32:11 -0500

    Add Exercise02.c to demonstrate variable usage

---
+++ C/Variable_and_Identifier/Excersise02.c
@@ -0,0 +1,15 @@
+// Replace ___ with your code
+#include <stdio.h>
+
+int main() {
+
+    // Create int variable named number
+    int number;
+    number =78;
+    
+
+    // Print the variable number
+ printf(" %d\n",number);
+
+    return 0;
+ }
