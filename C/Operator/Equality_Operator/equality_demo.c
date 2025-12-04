e0aed263a6820951b99ef1799b2504163fb6ab64
unknown User 404 <Bhavneet11d@gmail.com>
2025-12-04 15:07:39 -0500

    Create equality_demo.c

---
+++ C/Operator/Equality_Operator/equality_demo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 20;
+
+    printf("a == b : %d\n", a == b);
+    printf("a != b : %d\n", a != b);
+
+    int x = 5, y = 5;
+
+    printf("x == y : %d\n", x == y);
+    printf("x != y : %d\n", x != y);
+
+    return 0;
+}
